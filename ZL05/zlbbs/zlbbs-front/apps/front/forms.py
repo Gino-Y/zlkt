6f90425d830734bb1f88672d2a372594ba04add5
Gino-Y <ginoyan250@gmail.com>
2022-04-07 17:54:33 +0800

    字体文件

---
+++ ZL05/zlbbs/zlbbs-front/apps/front/forms.py
@@ -0,0 +1,11 @@
+from wtforms import Form
+from wtforms.fields import StringField
+from wtforms.validators import Email, Length, EqualTo
+
+class RegisterForm(Form):
+    email = StringField(validators=[Email(message='请输入正确的邮箱！')])
+    email_captcha = StringField(validators=[Length(6, 6, message='请输入正确格式的邮箱验证码！')])
+    username = StringField(validators=[Length(3, 20, message='请输入正确长度的用户名')])
+    password = StringField(validators=[Length(6, 20, message='请输入正确长度的密码')])
+    repeat_password = StringField(validators=[EqualTo('password', message='两次密码不一致！')])
+    graph_captcha = StringField(validators=[Length(4, 4, message='请输入正确长度的图形验证码！')])
